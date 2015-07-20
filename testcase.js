299d3568a1cdcb088ce110bc1ad6d2a08347c9f1
tapir <tapir.dream@gmail.com>
2015-07-20 20:37:20 +0800

    Update testcase.js

--- testcase.js
+++ testcase.js
@@ -1,8 +1,8 @@
 // testcase
-var reader = Reader(fs.readFile('C:/Users/Administrator/Desktop/test.html'));
-var d = new Date();
-var htmlTokenizer = HTMLTokenizer(reader);
-var tdl = htmlTokenizer.scanner();
+// var reader = Reader(fs.readFile('C:/Users/Administrator/Desktop/test.html'));
+// var d = new Date();
+// var htmlTokenizer = HTMLTokenizer(reader);
+// var tdl = htmlTokenizer.scanner();
 //console.log(tdl);
 //console.log(htmlTokenizer.toHTML(tdl));
 //console.log(htmlTokenizer.tabClosedCheck(tdl));
@@ -10,7 +10,7 @@ var tdl = htmlTokenizer.scanner();
 //console.log(tdl);
 //console.log(htmlTokenizer.toHTML(tdl));
 //var tdl = htmlTokenizer.HTMLFilter(tdl);
-console.log(tdl);
+// console.log(tdl);
 //tdl = htmlTokenizer.HTMLAttrValFilter(tdl);
 //console.log(htmlTokenizer.toHTML(tdl));
 //console.log(new Date() -  d);
